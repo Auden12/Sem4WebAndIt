e917e5c65af08443264f47e65a167175f555d371
Auden12 <137095274+Auden12@users.noreply.github.com>
2025-03-25 13:01:37 +0100

    more

---
+++ it sikkerhed/PasswordCrackerMaster2023/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+namespace PasswordCrackerMaster2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CrackerMaster master = new CrackerMaster();
+
+            master.Listen(IPAddress.Loopback, 6789);
+        }
+    }
+}
---
+++ it sikkerhed/PasswordCrackerSlave2023/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PasswordCrackerSlave2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Slave slave = new Slave();
+
+            slave.connect("localhost", 6789);
+
+            
+        }
+    }
+
+}
